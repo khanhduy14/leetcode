290bdfc7005bbe85119310aebbb46269eccb4ff6
duy.kieu <kieukhanhduy1@gmail.com>
2022-03-22 21:44:37 +0700

    818 + 1663

---
+++ P1663.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    string getSmallestString(int n, int k) {
+        string st(n, 'a');
+        k -= n;
+        for (int i = n - 1; k > 0; --i) {
+            int add = min(k, 25);
+            st[i] += add;
+            k -= add;
+        }
+        return st;
+    }
+};
+
+int main() {
+    Solution s;
+    cout << s.getSmallestString(5, 73);
+}
--- P818.cpp
+++ P818.cpp
@@ -5,7 +5,22 @@ using namespace std;
 class Solution {
 public:
     int racecar(int target) {
+        int dp[target + 1];
+        for (int i = 1; i <= target; ++i) {
+            dp[i] = INT32_MAX;
 
+            int m = 1, j = 1;
+            for (; j < i; j = (1 << ++m) - 1) {
+                for (int q = 0, p = 0;  p < j; p = (1 << ++q) - 1) {
+                    dp[i] = min(dp[i], m + 1 + q + 1 + dp[i - (j - p)]);
+                }
+            }
+
+            cout << i << " " << j << endl;
+
+            dp[i] = min(dp[i], m + (i == j ? 0 : 1 + dp[j - i]));
+        }
+        return dp[target];
     }
 };
 
